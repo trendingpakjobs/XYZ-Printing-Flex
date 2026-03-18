f4d755cea97e5461aa3aaa21f715398238349c51
trendingpakjobs <trendingpakjobs@gmail.com>
2026-03-18 21:35:16 +0500

    Add counter animation functionality in script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const counters = document.querySelectorAll('.counter');
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / 200; // speed
+    if(count < target){
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
